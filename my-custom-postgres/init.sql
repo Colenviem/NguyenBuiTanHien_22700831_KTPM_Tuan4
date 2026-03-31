9d0b4d5288c51137fda0232be68ecc658f462564
Colenviem <hiennguyenbuitan@gmail.com>
2026-03-31 11:17:58 +0700

    Bài 8: Tạo Dockerfile cho PostgreSQL tùy chỉnh

---
+++ my-custom-postgres/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (username, email) VALUES 
+('NguyenBuiTanHien', 'hien@example.com'),
+('DockerUser', 'docker@test.com');
