29e76873067b2fbc7718315d814650dad9cca3b3
Colenviem <hiennguyenbuitan@gmail.com>
2026-03-31 11:19:09 +0700

    Bài 10: Chạy ứng dụng PHP với Apache

---
+++ my-php-app/index.php
@@ -0,0 +1,5 @@
+<?php
+    echo "<h1>Chào mừng NguyenBuiTanHien đến với Docker PHP!</h1>";
+    echo "<p>Phiên bản PHP hiện tại: " . phpversion() . "</p>";
+    echo "<p>Thời gian hệ thống trong container: " . date('Y-m-d H:i:s') . "</p>";
+?>
